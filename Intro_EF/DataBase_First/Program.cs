d87de525176ce742855476cb211f2d3d16ddb6b6
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2019-02-21 19:36:58 +0400

    DataBase_First

---
+++ Intro_EF/DataBase_First/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBase_First
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var contex = new ShopDBEntities();
+            Console.WriteLine("Id  First Name    Last Name    Salary\n");
+            foreach (var item in contex.People)
+                           
+                {
+                    Console.WriteLine("{0,-2} {1,-12}  {2, -12}  {3}", item.EmployeeID, item.FName, item.LName, item.Salary);
+                }
+            
+        }
+    }
+}
